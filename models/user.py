a614e03270b5532cdeb4246e3ce307e63052aa67
Robert Odhiambo <odhizrober254@gmail.com>
2023-12-18 23:39:22 +0300

    city, user, places

--- models/user.py
+++ models/user.py
@@ -20,7 +20,6 @@ class User(BaseModel, Base):
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
         places = relationship("Place", backref="user")
-        reviews = relationship("Review", backref="user")
     else:
         email = ""
         password = ""
