bc9c0412031f67685c3fef98ffc7f999afb670e0
Robert Odhiambo <odhizrober254@gmail.com>
2024-01-11 15:42:46 +0300

    deploying static fioles

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+web server distributioni
+"""
+from fabric.api import *
+import os.path
+
+env.user = 'ubuntu'
+env.hosts = ["54.237.1.142", "52.91.132.134"]
+env.key_filename = "~/id_rsa"
+
+
+def do_deploy(archive_path):
+    """
+    deploys and distributes an archive to your web servers
+    """
+    if os.path.exists(archive_path) is False:
+        return False
+    try:
+        arc = archive_path.split("/")
+        base = arc[1].strip('.tgz')
+        put(archive_path, '/tmp/')
+        sudo('mkdir -p /data/web_static/releases/{}'.format(base))
+        main = "/data/web_static/releases/{}".format(base)
+        sudo('tar -xzf /tmp/{} -C {}/'.format(arc[1], main))
+        sudo('rm /tmp/{}'.format(arc[1]))
+        sudo('mv {}/web_static/* {}/'.format(main, main))
+        sudo('rm -rf /data/web_static/current')
+        sudo('ln -s {}/ "/data/web_static/current"'.format(main))
+        return True
+    except:
+        return False
