877c109f4cd19ccc5f4ff92721125b1d28f74c7b
Robert Odhiambo <odhizrober254@gmail.com>
2024-01-27 11:41:04 +0300

    Route: /python/ /python/<text>

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -27,6 +27,13 @@ def c_text(text):
     return "C {}".format(text)
 
 
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def py_text(text='is Cool'):
+    """ """
+    text = text.replace("_", " ")
+    return 'Python {}'.format(text)
+
 if __name__ == '__main__':
     """ """
     app.run(host='0.0.0.0', port=5000)
